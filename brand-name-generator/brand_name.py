bc291ddffb4cf9b8fb2e613207979e56d7328fb1
Hayden-C-Reynolds <Corell8803@gmail.com>
2025-09-26 06:05:46 -0500

    Create brand_name.py

---
+++ brand-name-generator/brand_name.py
@@ -0,0 +1,11 @@
+"""
+Band Name Generator
+-------------------
+A simple Python program that generates a band name by combining
+the user's city and pet name.
+"""
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
